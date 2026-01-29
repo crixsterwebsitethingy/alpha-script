e6d13693986eecdbe7a45d8ad51618a840a0b0ec
crix_0z <crixsteryt@gmail.com>
2026-01-29 08:42:22 +0200

    H

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,3 +1,5 @@
+import math
+
 def exec(data):
     print("--------------Execution Started--------------")
     for value in data:
@@ -13,6 +15,15 @@ def exec(data):
             print(value["LINE"], ": ", result)
         elif value["FUNC"] == "as.div":
             result = float(value['ARGS'][0]) / float(value['ARGS'][1])
+            print(value["LINE"], ": ", result)
+        elif value["FUNC"] == "sqrt":
+            result = math.sqrt(float(value['ARGS'][0]))
+            print(value["LINE"], ": ", result)
+        elif value["FUNC"] == "abs":
+            result = abs(float(value['ARGS'][0]))
+            print(value["LINE"], ": ", result)
+        elif value["FUNC"] == "pow":
+            result = pow(int(value['ARGS'][0]), int(value['ARGS'][1]))
             print(value["LINE"], ": ", result)
 
     print("--------------Execution Ended--------------")
